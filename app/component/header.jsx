9bf22622fbfa66eece1e5fd42b4df9b8e42f24f1
Faturado <billyyoukill25@gmail.com>
2025-03-13 15:17:41 +0800

    gfdfgdfgdfg

---
+++ app/component/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const header = () => {
+  return (
+    <main className="w-full h-full flex ">
+      <div className="w-100 h-100 bg-red-400 border-4 border-red-500 rounded-md justify-center flex items-center"></div>
+    </main>
+  );
+};
+
+export default header;
